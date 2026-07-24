3d4e11c0ca1c315314632c8c1de504333fbd75de
Stygian <263682819+StygianRivers@users.noreply.github.com>
2026-07-24 16:14:10 +0200

    userpersist and an attempt at save states

--- behind-the-scenes/README.md
+++ behind-the-scenes/README.md
@@ -16,17 +16,7 @@
 
 **To do: Python generator**
 - auto-bold tags ( @.* )
-- if preceding message is by the same user, remove username for current message
 - insert typing indications if message is long, automatic length based on user ID and message length, wrap in relatyping divs
   - if user = minato, count message characters and produce a set typing length based on that (but only for anything over 5 characters)
   - if user != minato, count message words(separated by spaces) and produce a set typing length based on that (but only for anything over 4 words)
-- insert custom typing indicators for each [#*] indicating hesitation, option of multiple typing indicators per [#*]
-  - leave out [string] indicators without a number at the start
-  - wrap multiple sibling indicators in a single relatyping div 
-- iterate each typing indication to add .t# class
-- per hmessage, input alt text that will become the default
-  - ideally the messages would appear one by one and let me type alt text for each, without having to format it manually at all, then immediately move on to the next message when I hit enter
-    - replacing the hmessage text with the alt text at that point
-- per message, input number to output transition delay (add to last message's or indicator's delay number)
-- per typing indication, input two numbers to output transition delay and blink duration
-
+- make sure time indicators on their own line aren't counted as messages
--- behind-the-scenes/bhind-generator.py
+++ behind-the-scenes/bhind-generator.py
@@ -2,24 +2,58 @@
 
 import re
 
+try:
+    old_data = open("olddata.txt", "r+")
+    old_data.read()
+except IOError:
+    print("No previous data found.")
+    input_file = input("Which file to generate for?\n")
+    output_file = re.match(r"(?:.*/)?(.+)\..*", input_file)
+    start = 0
+    typing_iteration = 0
+    delaylist = []
+    userpersist = 0
+
+with open(input_file, "r") as f:
+    contents = f.read()
+messages = contents.strip().split("\n\n")
+
+html = []
+css = []
+
+html_intro = """<div class="flex">
+        <details class="fadedetails">
+            <summary>Wild Card Support Group<span class="overlay"></span></summary>
+            <div class="phone">
+                <h3 class="phoneheader">
+                    <span class="hiddeninfo">Group chat: </span>
+                    Wild Card Support Group
+                </h3> 
+                """
+
+html_end = """
+            </div>
+        </details>
+    </div>
+    """
+
+html.append(html_intro)
+
 def rmbrackets(onemessage): 
-    regmatch = re.match(r"(\[(\d)\s?min\])?(\*\*(.*):\*\*)? ?(.*)?", onemessage)
+    regmatch = re.match(r"(\[(\d)?\s?.*\])?(\*\*(.*):\*\*)? ?(.*)?", onemessage)
     matchone = regmatch[2]
     matchtwo = regmatch[4]
     matchthree = regmatch[5]
     return matchone, matchtwo, matchthree
 
-with open("example.md", "r") as f:
-    contents = f.read()
-
-messages = contents.strip().split("\n\n")
-
-delaylist = []
-
-html = []
-css = []
-
-typing_iteration = 0
+# def boldtags(boldmessage):
+#     nametag = re.match(r"( @.* )?", boldmessage)
+#     print(nametag[1])
+#     if nametag == None:
+#         print("No tags")
+#     else:
+#         re.sub(r"( ).*", " <strong>", nametag[1])
+#         re.sub(r".*( )", "</strong> ", nametag[1])
 
 def trycss(cclass, cmessage, ctime):
     while True:
@@ -46,32 +80,48 @@ def trycss(cclass, cmessage, ctime):
 def tryhtml(huser, hmessage, hclass):
     print('\033[96m' + "Enter alt text:" + '\x1b[0m')
     baseMessage = input()
-    return f"""
+    # if the username is the same as the previous message, don't add the username for this one as well
+    global userpersist 
+    if userpersist == huser:
+        return f"""<p class="text visibly m{hclass}"><span class="fallback">{baseMessage}</span></p>"""
+    else:
+        userpersist = huser
+        return f"""
 <p class="username visibly m{hclass}"><strong>{huser}</p></strong></p>
 <p class="text visibly m{hclass}"><span class="fallback">{baseMessage}</span></p>
 """
 
 def typingdots():
-    html.append("<div class="relatyping">")
+    html.append("<div class=\"relatyping\">")
     user_typing = input("Who is typing?")
     typing_iter = typing_iteration + 1
     while True:
+        typing_length = input("How long does the indicator last?\n")
+        ty_start = input("How long is the pause before the indicator appears?\n")
+        delaylist.append(int(ty_start))
+        totaldelay = sum(delaylist)
+        delaylist.append(int(typing_length))
         ty_html = f"""
         <p class="text visibly is-typing"><span class="fade-typing"><span class="t{typing_iter}"><span class="texthide"><strong><small>{user_typing} is typing...</small></strong></span></span></span></p>
     """
-        more_typing = input("Another blink?")
+        ty_css = f"""#workskin:has(.fadedetails[open]) .t{typing_iter} {{
+    visibility: visible;
+    transition: all {typing_length}s linear {totaldelay}s;
+}}"""
+        css.append(ty_css)
+        more_typing = input("Another blink?\n")
         if more_typing == 'y' or 'yes' or 'ok':
-            typing_iter = typing_iter + 1
-            continue
+             typing_iter = typing_iter + 1
+             continue
         elif more_typing == 'n' or 'no' or 'quit':
-            html.append(ty_html)
-            html.append("</div>")
-            break
-    #css
+             html.append(ty_html)
+             html.append("</div>")
+             break
 
-for i, msg in enumerate(messages):
+for i, msg in enumerate(messages[start:]):
   print('\033[31m' + msg + '\x1b[0m')
   (time, user, message) = rmbrackets(msg)
+  #boldtags(message)
   try:
       int(time)
       hesitation = int(time) * 60
@@ -87,7 +137,6 @@ for i, msg in enumerate(messages):
     if confirm == '':
         html.append(msg_html)
         css.append(msg_css)
-        break
     elif confirm == 'skip':
         break
     elif confirm == 'retry':
@@ -98,23 +147,36 @@ for i, msg in enumerate(messages):
     elif confirm == 'css' or 'time' or 'delay' or 'int':
         msg_css = trycss(i, message)
     else: 
-        print("Type \'skip\' to skip this message, or 'retry' to change the input.")
-  while True:
-      print("Typing indicator flashing?")
-      typing_indicator = input().lower()
-      if typing_indicator == 'y' or 'yes' or 'ok':
-         (typing_html, typing_css, typing_iteration) = typingdots
-      else:
+        print("Type 'skip' to skip this message, or 'retry' to change the input.")
+    print("Typing indicator flashing?")
+    typing_indicator = input().lower()
+    if typing_indicator == 'y' or 'yes' or 'ok': #why tf does this not work
+        typingdots()
+    else:
+        continue
+    save_progress = input("'Y' to save and quit").lower()
+    if save_progress == 'y' or 'yes' or 'ok':
+        open(old_data, "w")
+        old_data.write("input_file = " + input_file + "\noutput_file = " + output_file + "\nstart = " + start + "\ntyping_iteration = " + typing_iteration + "\ndelaylist = " + delaylist + "userpersist = " + userpersist)
+        old_data.close()
+        finalhtml = "\n".join(html)
+        finalcss = "\n".join(css)
+        with open (f"{output_file[1]}.html", "a") as fohtml:
+            print(finalhtml, file=fohtml) 
+        with open ("output.css", "a") as focss:
+            print(finalcss, file=focss)
+    else:
         break
 
+html.append(html_end)
+
 finalhtml = "\n".join(html)
-print(finalhtml)
-
 finalcss = "\n".join(css)
-print(finalcss)
 
-with open ("output.html", "w") as fohtml:
+with open (f"{output_file[1]}.html", "a") as fohtml:
     print(finalhtml, file=fohtml) 
 
-with open ("output.css", "w") as focss:
+with open ("output.css", "a") as focss:
     print(finalcss, file=focss)
+
+print("Done!")
